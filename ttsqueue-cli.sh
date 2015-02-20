035fcf45cd9464a3e6fb1a05fc71041c39186cb6
prodigy7 <opensource@prodigy7.de>
2015-02-20 15:45:22 +0100

    Initial import

---
+++ ttsqueue-cli.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+# Define ttsqueue root
+TTSROOT=$(dirname $0)
+
+# Do some checks
+if [ ! -f $TTSROOT/ttsqueue.conf ]; then
+	echo -e ""
+	echo -e "ERROR: Config file ttsqueue.conf in $TTSROOT not found"
+	echo -e ""
+	exit 10;
+else
+
+	# Load configuration
+	source $TTSROOT/ttsqueue.conf
+
+	# Check: Variable defined
+	if [ ! $TTSQUEUE ]; then
+		echo -e ""
+		echo -e "ERROR: TTSQUEUE not defined in ttsqueue.conf"
+		echo -e ""
+		exit 11;
+	fi
+
+	# Check: Path exist
+	if [ ! -d $TTSQUEUE ]; then
+		echo -e ""
+		echo -e "ERROR: Queue path $TTSQUEUE does not exist"
+		echo -e ""
+		exit 12;
+	fi
+
+	# Check: User can write
+	if [ ! -w $TTSQUEUE ]; then
+		echo -e ""
+		echo -e "ERROR: Queue path $TTSQUEUE is not writeable by current user"
+		echo -e ""
+		exit 13;
+	fi
+fi
+
+# Some default defintions
+TTS=""
+
+# Function for displaying help
+function usage {
+	echo -e ""
+	echo -e "Usage:"
+	echo -e " ttsqueue-cli.sh action parameter"
+	echo -e ""
+	echo -e "Actions:"
+	echo -e ""
+	echo -e "  add\t\t\tAdd text to text-to-speach queue."
+	echo -e "\t\t\tParameter is the text to be spoken. Script returns the id of the queued text."
+	echo -e ""
+	echo -e "\t\t\tExample:"
+	echo -e "\t\t\t\tuser@pc:/home/user>ttsqueue-cli.sh add \"This is a test\""
+	echo -e "\t\t\t\t7263"
+	echo -e "\t\t\t\tuser@pc:/home/user>"
+	echo -e ""
+	echo -e "  check\t\t\tCheck text is already spoken."
+	echo -e "\t\t\tScript returns exit codes: 0 = not spoken, 1 = spoken/removed."
+	echo -e ""
+	echo -e "\t\t\tExample:"
+	echo -e "\t\t\t\tuser@pc:/home/user>ttsqueue-cli.sh check 7263"
+	echo -e "\t\t\t\tuser@pc:/home/user>echo \$?"
+	echo -e "\t\t\t\t0"
+	echo -e "\t\t\t\tuser@pc:/home/user>"
+	echo -e ""
+	echo -e "  remove\t\tRemove text from text-to-speach queue."
+	echo -e "\t\t\tParameter is the id which was returned during action add. Script returns exit codes: 0 = success, 1 = failed (text maybe already spoken)."
+	echo -e ""
+	echo -e "\t\t\tExample:"
+	echo -e "\t\t\t\tuser@pc:/home/user>ttsqueue-cli.sh remove 7263"
+	echo -e "\t\t\t\tuser@pc:/home/user>echo \$?"
+	echo -e "\t\t\t\t1"
+	echo -e "\t\t\t\tuser@pc:/home/user>"
+	echo -e ""
+}
+
+# Handling with parameters
+case $1 in
+
+	# Handle parameter add
+	add)
+
+		# If no text given, return error
+		if [ "$2" == "" ] ; then
+			echo -e ""
+			echo -e "ERROR: No text for speach given"
+			echo -e ""
+			exit 14;
+
+		# If text given, queue
+		else
+			TTS="$2"
+			echo "$TTS" > $TTSQUEUE/$$
+			chown $TTSUSER $TTSQUEUE/$$
+			echo $$
+			exit 0;
+		fi
+	;;
+
+	# Handle parameter remove
+	remove)
+
+		# If no id given, return error
+		if [ "$2" == "" ] ; then
+			echo -e ""
+			echo -e "ERROR: No text id given"
+			echo -e ""
+			exit 15
+
+		# Handle remove of text
+		else
+
+			# If text exists/is removable, do it
+			if [ -w $TTSQUEUE/$2 ] ; then
+				rm $TTSQUEUE/$2 >/dev/null 2>&1
+				exit 0;
+
+			# If text already spoken/not removeable, return error code
+			else
+				exit 1;
+			fi
+		fi
+	;;
+
+	# Handle parameter remove
+	check)
+
+		# If no id given, return error
+		if [ "$2" == "" ] ; then
+			echo -e ""
+			echo -e "ERROR: No text id given"
+			echo -e ""
+			exit 15
+
+		# Handle remove of text
+		else
+
+			# If text exists, report it
+			if [ -f $TTSQUEUE/$2 ] ; then
+				exit 0;
+
+			# If text already spoken, report it
+			else
+				exit 1;
+			fi
+		fi
+	;;
+
+	*)
+		usage;
+		exit 20;
+	;;
+esac
+
+exit 0
---
+++ ttsqueue-daemon.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+
+# Check -> needed package installed?
+which inotifywait >/dev/null 2>&1;
+if [ "$?" -eq 1 ]; then
+	echo -e ""
+	echo -e "ERROR: Needed command inotifywait not found. Please install package inotify-tools."
+	echo -e ""
+	exit 1;
+fi
+
+# Define ttsqueue root
+TTSROOT=$(dirname $0)
+
+# Do some checks
+if [ ! -f $TTSROOT/ttsqueue.conf ]; then
+	echo -e ""
+	echo -e "ERROR: Config file ttsqueue.conf in $TTSROOT not found"
+	echo -e ""
+	exit 10;
+else
+
+	# Load configuration
+	source $TTSROOT/ttsqueue.conf
+
+	# Check: Given user
+	if [ "$TTSUSER" == "root" ]; then
+		echo -e ""
+		echo -e "ERROR: TTSUSER can not be root! Please choose another user and try it again"
+		echo -e ""
+		exit 11;
+	else
+		TTSUSERID=$(cat /etc/passwd | grep $TTSUSER | cut -d ":" -f4)
+		if [ "$?" -ne 0 ] ; then
+			echo -e ""
+			echo -e "ERROR: TTSUSER $TTSUSER not found"
+			echo -e ""
+			exit 12;
+		fi
+	fi
+
+	# Check: Variable defined
+	if [ ! $TTSDELAY ]; then
+		echo -e ""
+		echo -e "ERROR: TTSDELAY not defined in ttsqueue.conf"
+		echo -e ""
+		exit 13;
+	fi
+
+	if [ ! $TTSQUEUE ]; then
+		echo -e ""
+		echo -e "ERROR: TTSQUEUE not defined in ttsqueue.conf"
+		echo -e ""
+		exit 14;
+	fi
+
+	# Check: FilePath exist
+	if [ ! -f $TTSINTRO ]; then
+		echo -e ""
+		echo -e "ERROR: Intro file $TTSINTRO does not exist"
+		echo -e ""
+		exit 15;
+	fi
+
+	if [ ! -d $TTSQUEUE ]; then
+		echo -e ""
+		echo -e "ERROR: Queue path $TTSQUEUE does not exist"
+		echo -e ""
+		exit 16;
+	fi
+
+	# Check: User can write
+	if [ ! -w $TTSQUEUE ]; then
+		echo -e ""
+		echo -e "ERROR: Queue path $TTSQUEUE is not writeable by user $TTSUSER"
+		echo -e ""
+		exit 17;
+	fi
+
+	if [ -f $TTSROOT/ttsqueue.log ]; then
+		if [ ! -w $TTSROOT/ttsqueue.log ]; then
+			echo -e ""
+			echo -e "ERROR: Log file $TTSROOT/ttsqueue.log is not writeable by user $TTSUSER"
+			echo -e ""
+			exit 18;
+		fi
+	fi
+fi
+
+
+# Function for displaying help
+function usage {
+	echo -e ""
+	echo -e "Usage:"
+	echo -e " ttsqueue-daemon.sh [start|stop]"
+	echo -e ""
+	echo -e "Actions:"
+	echo -e ""
+	echo -e "  start\t\t\tStart ttsqueue daemon."
+	echo -e "  stopt\t\t\tStop ttsqueue daemon."
+	echo -e ""
+}
+
+function monitoringQueue {
+
+	# Start watching
+	inotifywait -q -m -e close_write "$TTSQUEUE" | while read DIRECTORY EVENTLIST EVENTFILE; do
+
+		# Build full qualified path
+		FILEPATH="$DIRECTORY/$EVENTFILE"
+
+		# Special: If file "stop" is created, stop daemon
+		if [ "$EVENTFILE" == "stop" ] ; then
+			echo -e "Daemon stopped"
+
+			# Remove stop file
+			rm $FILEPATH >/dev/null 2>&1
+			exit 0
+		else
+			# Some output for log file
+			date
+			cat $DIRECTORY/$EVENTFILE;
+
+			# Encode text string
+			SPEAKTEXT=$(cat $DIRECTORY/$EVENTFILE | od -An -tx1 | tr ' ' % | xargs printf "%s")
+
+			# Build TTS Request
+			TTSREQUEST=$(echo $TTSURL | sed "s/%SPEAKTEXT%/$SPEAKTEXT/g")
+
+			# Play
+			if [ -z "$TTSINTRO" ]; then
+				cvlc $TTSPLAYPARAM "$TTSREQUEST"
+			else
+				cvlc $TTSPLAYPARAM "$TTSINTRO" "$TTSREQUEST"
+			fi
+
+			echo -e ""
+
+			# Now remove file
+			if [ -w $DIRECTORY/$EVENTFILE ] ; then
+				rm $FILEPATH >/dev/null 2>&1
+
+				# Wait before next can be played
+				sleep $TTSDELAY
+			fi
+		fi
+	done
+}
+
+# Handling with parameters
+case $1 in
+
+	# Start daemon
+	start)
+
+		# Check is already running
+		if [ -f $TTSROOT/ttsqueue.pid ] ; then
+			PID=$(cat $TTSROOT/ttsqueue.pid | sed "s/\n//g")
+			ps -ef | grep $PID >/dev/null
+			if [ "$?" -eq 0 ] ; then
+				echo "Daemon is already running"
+				exit 2;
+			else
+				rm $TTSROOT/ttsqueue.pid >/dev/null 2>&1
+			fi
+		fi
+
+		# Cleanup queue
+		rm $TTSQUEUE/* >/dev/null 2>&1
+
+		nohup sudo -u $TTSUSER $0 daemon > $TTSROOT/ttsqueue.log 2>&1 &
+		if [ "$?" -eq 0 ] ; then
+
+			# Write pid to file
+			echo $! > $TTSROOT/ttsqueue.pid
+
+			# Print status
+			echo -e "Daemon started"
+		fi
+	;;
+
+	# Get status
+	status)
+
+		# Check is already running
+		if [ -f $TTSROOT/ttsqueue.pid ] ; then
+			PID=$(cat $TTSROOT/ttsqueue.pid | sed "s/\n//g")
+			ps -ef | grep -v "grep" | grep $PID  >/dev/null
+			if [ "$?" == "0" ] ; then
+				echo -e "Daemon running"
+				exit 0
+			else
+				echo "Daemon is not running"
+				exit 2;
+			fi
+		else
+			echo -e "Daemon is not running"
+			exit 3;
+		fi
+
+	;;
+
+	# Stop daemon
+	stop)
+
+		# Check is already running
+		if [ -f $TTSROOT/ttsqueue.pid ] ; then
+			PID=$(cat $TTSROOT/ttsqueue.pid | sed "s/\n//g")
+			ps -ef | grep -v "grep" | grep $PID >/dev/null
+			if [ "$?" == "0" ] ; then
+				echo -e "Daemon stopped"
+				echo "$TTS" > $TTSQUEUE/stop
+
+				# Remove pid
+				rm $TTSROOT/ttsqueue.pid >/dev/null 2>&1
+
+				# Kill this and all child processes
+				kill -- -$(ps -o pgid=$$ | grep -o '[0-9]*')
+
+				exit 0
+			else
+				echo "Daemon is not running"
+				rm $TTSROOT/ttsqueue.pid >/dev/null 2>&1
+				exit 2;
+			fi
+		else
+			echo -e "Daemon is not running"
+			exit 3;
+		fi
+
+	;;
+
+	daemon)
+		monitoringQueue;
+		exit 0;
+	;;
+
+	*)
+		usage;
+		exit 20;
+	;;
+esac
+
+exit 0
